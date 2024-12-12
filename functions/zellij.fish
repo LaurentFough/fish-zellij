06a9730cced84de61b2c49fb0349def59681fdb9
Łaurent ʘ❢Ŧ Ŧough <LaurentFough@users.noreply.github.com>
2024-12-12 11:06:45 -0800

    Update zellij.fish

--- functions/zellij.fish
+++ functions/zellij.fish
@@ -1,11 +1,11 @@
-function zellij-auto-start
+function __zellij::auto-start
     set -q ZELLIJ_PANE_ID && return
 
     set -U ZELLIJ_DISABLE_AUTO_EXIT_ONLY_ONCE false
 
-    set current_number (zellij list-sessions | rg "automatic-(\w+)" -or '$1' | sort -rV | head -n 1)
+    set current_number ( zellij list-sessions | rg "automatic-(\w+)" -or '$1' | sort -rV | head -n 1 )
     test -z "$current_number" && set current_number 0
-    set new_number (math "$current_number" + 1)
+    set new_number ( math "$current_number" + 1 )
 
     if $ZELLIJ_AUTO_START
         zellij -s "automatic-$new_number" || exit
@@ -19,21 +19,21 @@ function zellij-auto-start
 end
 
 
-function zellij-disable-auto-exit
+function __zellij::auto-exit:disable
     set -U ZELLIJ_AUTO_EXIT false
 end
 
 
-function zellij-disable-auto-start
+function __zellij::auto-start:disable
     set -U ZELLIJ_AUTO_START false
 end
 
 
-function zellij-enable-auto-exit
+function __zellij::auto-exit:enable
     set -U ZELLIJ_AUTO_EXIT true
 end
 
 
-function zellij-enable-auto-start
+function __zellij::auto-start:enable
     set -U ZELLIJ_AUTO_START true
 end
